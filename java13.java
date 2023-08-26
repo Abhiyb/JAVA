880cbd82e14a477abf8c7bd77b6f434afcbd2f7e
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-08-26 20:24:58 +0530

    Create java13.java

---
+++ java13.java
@@ -0,0 +1,14 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+// 		System.out.println("Hello World");
+   String str="name my is abhi";
+   String []word=str.split(" ");String temp=" ";
+   for(int i=word.length-1;i>=0;i--)
+   {
+       System.out.print(word[i]+" ");
+   }
+
+	}
+}
