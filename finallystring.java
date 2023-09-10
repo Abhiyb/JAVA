80f8a85a3e678abc5b402feb1b17e4c7ba5ed80d
Abhiyb <115642405+Abhiyb@users.noreply.github.com>
2023-09-10 13:56:03 +0530

    Create finallystring.java

---
+++ finallystring.java
@@ -0,0 +1,16 @@
+//==
+//.equals()
+//.compareToIgnoreCase()
+//.compareTo()
+//.replace('','')
+//.replaceAll('','')
+//.split('')
+// .indexOf()
+//.lastIndexOf('')
+//.charAt()
+//String.valueOf()  
+//   char[] charArray = str.toCharArray();
+// System.out.println(String.join(":","abc","b"));
+//=======boolean===
+//.startsWith()
+//.contains()
